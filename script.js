7278f2d01f174af489872ac08f9fa872e6850c7e
AP <apratimsanyal@gmail.com>
2023-02-12 14:35:14 -0800

    Final assignment will solutions to all exercises

--- script.js
+++ script.js
@@ -1,22 +1,212 @@
 //STEP 1
+
+/*
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+
+
 //STEP 2
+
+
+/*
+5.5                     // numeric literal expression
+'My name is Bob.'       // string literal expression
+false                   // boolean literal expression
+null                    // null literal expression
+*/
+
+
 //STEP 3
+
+/*
+(10 + 2)* 5     //example 1
+temp >= 32      //example 2
+*/
+
 //STEP 4
+
+/*
+let firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou
+*/
+
 //STEP 5
+
+/*
+
+let firstName
+firstName = "AP"
+
+let lastName = "Sanyal"
+
+let address, city, state, zipCode
+city = "Rochester"
+state = "MN"
+zipCode = 55901
+address = `${city}, ${state}, ${zipCode}`
+
+*/
+
 //STEP 6
+
+/*
+let x =3 
+let y="4"
+x= x + Number(y)
+console.log(x)          //returns numerical 7
+*/
+
+
 //STEP 7
+
+/*
+let statement
+let phrase = "This fact is"
+let fact = false
+statement = `${phrase} ${fact}`
+console.log(statement)          //returns 'This fact is false'
+
+let total
+let x = 10
+let y = true
+total = x+y
+console.log(total)              //returns 11
+*/
+
+
 //STEP 8
+
+/*
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString) 
+
+// added escape sequence
+
+*/
+
 //STEP 9
+
+/*
+let answer = null;
+console.log(answer);
+*/
+
 //STEP 10
+
+/*
+typeof 'Hello World'
+typeof 7
+typeof true
+typeof []
+typeof undefined
+*/
+
+
 //STEP 11
+
+/*
+alert('Hello ' + 'AP Sanyal' + ', welcome to the JavaScript class!')
+*/
+
+
 //STEP 12
+
+/*
+let name = 'AP Sanyal'
+alert('Hello ' + name + ', welcome to the JavaScript class!')
+*/
+
+
 //STEP 13
+
+/*
+
+let name = 'AP Sanyal'
+let course = 'JavaScript'
+alert(`Hello ${name}, welcome to the ${course} class!`)
+
+*/
+
 //STEP 14
+
+/*
+let name = 'AP Sanyal'
+let course = 'JavaScript'
+alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+*/
+
+
 //STEP 15
+
+/*
+let name = prompt('What is your name?')
+let course = 'JavaScript'
+alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+*/
+
+
 //STEP 16
+
+/*
+let name = prompt('What is your name?')
+let course = prompt('What class are you taking?')
+alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+*/
+
 //STEP 17
+
+/*
+let x = 10
+let y = 20
+console.log (x+y)
+*/
+
+
 //STEP 18
+
+/*
+let x = 20
+x +=20
+console.log(x)
+*/
+
+
 //STEP 19
+
+/*
+let x = 20
+x *=5
+console.log(x)
+*/
+
+
 //STEP 20
+
+/*
+let x = 20%3
+x= x/1
+console.log(x)
+*/
+
+
 //STEP 21
+
+/*
+let x=10
+let y=20
+let z=30
+console.log(x<y && x<z)
+*/
+
+
 //STEP 22
+
+/*
+let firstName = 'Belinda'
+let secondName = 'Bertha'
+console.log(firstName == secondName || firstName.length == secondName.length )
+*/
+
